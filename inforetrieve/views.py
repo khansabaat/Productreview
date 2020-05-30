56313c51f4f6601e455de75eae6f9bbb0a47ffde
Khan Sabaat <sabaat@botbakery.io>
2020-05-30 21:39:29 +0530

    Add API for text search

--- inforetrieve/views.py
+++ inforetrieve/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from elasticsearch import Elasticsearch
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class Search(APIView):
+    def get(self, request):
+        query = {
+            "query": {
+                "match": {
+                    "review/text": request.data["text"]
+                }
+            },
+            "sort": [
+                {"review/score": {"order": "desc"}},
+                {"review/helpfulness": {"order": "desc"}}
+            ],
+            "highlight": {
+                "fields": {
+                    "review/text": {}
+                }
+            }
+        }
+        es = Elasticsearch()
+        res = es.search(query, "reviews")
+        if res["hits"]["total"]["value"]:
+            doc = res["hits"]["hits"][0]
+            highlighttext = doc["highlight"]["review/text"][0]
+            return Response(highlighttext)
+        return Response(status=204)
+
